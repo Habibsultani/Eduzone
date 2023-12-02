a8280f05d469c60075093c2cb5b242286fb46c7e
Habibsultani <a.habibsultani@gmail.com>
2023-12-02 22:29:31 +0300

    new get start button

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -73,11 +73,22 @@
               Cumque deleniti
             </p>
             <div class="flex gap-4">
-              <button
-                @click="GoToStartPage('/register')"
-                class="w-32 px-4 py-2 bg-blue-500 text-white active:bg-blue-700 hover:bg-blue-600 rounded-lg text-xl font-bold font-vazirmtn"
-              >
-                Get Start
+              <button class="cssbuttons-io-button">
+                Get started
+                <div class="icon">
+                  <svg
+                    height="24"
+                    width="24"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M0 0h24v24H0z" fill="none"></path>
+                    <path
+                      d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                </div>
               </button>
               <button
                 @click="GoToStartPage('/about')"
