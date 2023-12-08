412bf53243293e0da95570d305eff4b783db8cf8
Habibsultani <a.habibsultani@gmail.com>
2023-12-08 12:43:41 +0300

    new style

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -7,7 +7,7 @@
     <section class="md:max-w-[1280px] mx-auto hidden md:block px-5">
       <!-- The hero section  -->
       <div id="hero-section" class="h-screen">
-        <header class="container max-w-[1280px] mx-auto px-5 fixed">
+        <header class="container max-w-[1280px] mx-auto px-5 fixed z-10">
           <nav class="py-6 flex justify-between align-center">
             <!-- Here is the logo place -->
             <h2 class="text-center text-gray-700 text-xl font-logo">Eduzone</h2>
